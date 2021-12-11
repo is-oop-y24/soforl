82556046323bbf82cfc86f6a2c31b10956af8313
Софья Орлова <orlova.sofia2002@gmail.com>
2021-12-11 03:25:34 +0300

    refactor: update tests

--- BackupsExtra.Tests/BackupExtraTests.cs
+++ BackupsExtra.Tests/BackupExtraTests.cs
@@ -35,22 +35,7 @@ namespace BackupsExtra.Tests
             _backupManager.CleanPoints(new AmountCleaning(), null,2);
             Assert.AreEqual(_backupManager.BackupJob.RestorePoints.Count, 2);
         }
-        
-        [Test]
-        public void CleanRestorePoints_DateAlgorithm()
-        {
-            JobObject jobObject1 = new JobObject(new FileInfo(@"../../../FilesExtra/File_A"));
-            JobObject jobObject2 = new JobObject(new FileInfo(@"../../../FilesExtra/File_B"));
-            _backupManager.AddJobObject(jobObject1);
-            _backupManager.AddJobObject(jobObject2);
-            _backupManager.BeginBackup(new SplitStorage());
-            DateTime date1 = _backupManager.BackupJob.RestorePoints[0].DateBackup;
-            _backupManager.BeginBackup(new SplitStorage());
-            DateTime date2 = _backupManager.BackupJob.RestorePoints[1].DateBackup;
-            _backupManager.CleanPoints(new DateCleaning(), date1);
-            Assert.AreEqual(_backupManager.BackupJob.RestorePoints.Count, 2);
-        }
-        
+
         [Test]
         public void MergeRestorePoints_SingleStorage()
         {
